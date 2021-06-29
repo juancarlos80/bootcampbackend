3d45a9a7438f6e015833235641c23af59172af6a
juancarlos80 <juan.k.80@gmail.com>
2021-06-29 16:34:15 -0400

    Casting anonymous types with generic

---
+++ AnonymousTypes/AnonymousTypes/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+namespace AnonymousTypes
+{
+    
+
+    class Anonymous
+    {
+
+        public object getData()
+        {
+            return new { Name = "pepe", EmailID = "pepe@gmail.com" };
+        }
+
+        private static T Cast<T>(T typeHolder, Object x)
+        {            
+            return (T)x;
+        }
+
+        static void Main(string[] args)
+        {
+
+            Anonymous anony = new Anonymous();
+            dynamic anonymousDynamicData = anony.getData();
+            Console.WriteLine(string.Format("{0} {1}", anonymousDynamicData.Name, anonymousDynamicData.EmailID) );
+
+            object anonymousData = anony.getData();
+
+            var obj = new { Name = "", EmailID = "" };
+            obj = Cast(obj, anonymousData);
+
+            Console.WriteLine(string.Format("{0} {1}", obj.Name, obj.EmailID));
+            
+        }
+        
+
+    }
+}
