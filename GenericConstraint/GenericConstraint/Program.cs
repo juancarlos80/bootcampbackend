58688e51e253dd8c4b2338f8441b5c2962c3f308
juancarlos80 <juan.k.80@gmail.com>
2021-06-29 16:32:01 -0400

    Generic Singleton exercise

---
+++ GenericConstraint/GenericConstraint/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace GenericConstraint
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {            
+
+            Person.Unique.Name = "Pepe";
+            Console.WriteLine( Person.Unique.Name );            
+        }
+        
+    }    
+
+    public static class Unique 
+    { 
+        public static string Name { get; set; }
+    }
+
+
+    class UniqueInstance<T> where T : new()
+    {     
+        private static T _Unique = new();
+     
+        public static T Unique {
+            get { 
+                return _Unique;                
+            }
+            set {                
+                _Unique = value;
+            }
+        }
+
+    }
+
+    class Person : UniqueInstance<Person>
+    {
+        public string Name;
+    }
+
+}
