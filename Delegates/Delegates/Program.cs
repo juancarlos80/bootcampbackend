ef45d88b15ded29a84dc7784211a2b557d135398
juancarlos80 <juan.k.80@gmail.com>
2021-07-08 15:03:46 -0400

    Delegates excercise

---
+++ Delegates/Delegates/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Delegates
+{
+
+    public class Expression
+    {
+        private int first;
+        private int second;
+
+        public Expression(int first, int second)
+        {
+            this.first = first;
+            this.second = second;
+        }
+
+        public int Sum()
+        {
+            return first + second;
+        }
+
+        private int Substract()
+        {
+            return first - second;
+        }
+
+        private int Multiply()
+        {
+            return first * second;
+        }
+
+        public int ApplyOperator(Operation operation)
+        {
+            return operation();            
+        }
+    }
+
+
+    public delegate int Operation();
+
+
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Expression expresion = new Expression(10, 20);            
+
+            var result = expresion.ApplyOperator(expresion.Sum);
+            Console.WriteLine($"Result: {result}");
+        }
+    }
+}
