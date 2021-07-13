b2a1314d216925a497d5968f923da23c252dfef5
juancarlos80 <juan.k.80@gmail.com>
2021-07-13 11:21:42 -0400

    Extension function example

---
+++ ExtensionFunction/ExtensionFunction/MyCustomExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomFunctions
+{
+    public static class MyCustomExtensions
+    {
+        public static string reverseString(this String str)
+        {
+            char[] charArray = str.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+    }
+}
---
+++ ExtensionFunction/ExtensionFunction/Program.cs
@@ -0,0 +1,17 @@
+﻿    using System;
+
+namespace ExtensionFunction
+{
+    using CustomFunctions;
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Extension function example");
+
+            string text = "Paralelepipedo";
+            Console.WriteLine(text.reverseString());
+        }
+    }
+}
