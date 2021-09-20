6b7e51b7135644e2ddd1e1b5852cfc3ec3335928
juancarlos80 <juan.k.80@gmail.com>
2021-09-20 16:51:00 -0400

    MicroKernel Example

---
+++ Microkernel/MicrokernelHandsOn.Core/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MicrokernelHandsOn.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Init prog");
+            var factory = new PluginFactory();
+            var plugins = factory.GetPlugins();
+
+            foreach (var plugin in plugins)
+            {
+                plugin.SaySomething();
+            }
+
+            Console.WriteLine("End prog");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Microkernel/MicrokernelHandsOn.PluginA/CoolPlugin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using MicrokernelHandsOn.Contract;
+
+namespace MicrokernelHandsOn.Plugin
+{
+    public class CoolPlugin : IPlugin
+    {
+        public void SaySomething()
+        {
+            Console.WriteLine("Hello Cool Microkernel");
+        }
+    }
+}
